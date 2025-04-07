431df1b02789dc4e543bd9135c0d31d48559713c
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-04-07 09:29:17 -0300

    codigo

---
+++ ex01.py
@@ -0,0 +1,29 @@
+numeroMes= int(input("digite o numero do mes [entre 1 a 12]: "))
+if numeroMes == 1:
+    print("janeiro")
+elif numeroMes ==2:
+  print("fevereiro")
+elif numeroMes == 3:
+  print("março")
+elif numeroMes == 4:
+  print("abril")
+elif numeroMes == 5:
+  print("maio")
+elif numeroMes == 6:
+  print("junho")
+elif numeroMes == 7:
+  print("julho")
+elif numeroMes == 8:
+  print("agosto")
+elif numeroMes == 9:
+  print("setembro")
+elif numeroMes == 10:
+  print("outubro")
+elif numeroMes == 11:
+  print("novembro")
+elif numeroMes == 12:
+  print("dezembro")
+else:
+ print("DIGITE DE 1 A 12 SEU ANIMAL!!")
+
+
