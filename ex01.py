a7d747e1bd981f10726647e5d91f3600901b303b
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-04-07 09:39:36 -0300

    alteração

--- ex01.py
+++ ex01.py
@@ -1,27 +1,28 @@
 numeroMes= int(input("digite o numero do mes [entre 1 a 12]: "))
-if numeroMes == 1:
+if numeroMes >=1 and numeroMes<=12:
+ if numeroMes == 1:
     print("janeiro")
-elif numeroMes ==2:
+ elif numeroMes ==2:
   print("fevereiro")
-elif numeroMes == 3:
+ elif numeroMes == 3:
   print("março")
-elif numeroMes == 4:
+ elif numeroMes == 4:
   print("abril")
-elif numeroMes == 5:
+ elif numeroMes == 5:
   print("maio")
-elif numeroMes == 6:
+ elif numeroMes == 6:
   print("junho")
-elif numeroMes == 7:
+ elif numeroMes == 7:
   print("julho")
-elif numeroMes == 8:
+ elif numeroMes == 8:
   print("agosto")
-elif numeroMes == 9:
+ elif numeroMes == 9:
   print("setembro")
-elif numeroMes == 10:
+ elif numeroMes == 10:
   print("outubro")
-elif numeroMes == 11:
+ elif numeroMes == 11:
   print("novembro")
-elif numeroMes == 12:
+ elif numeroMes == 12:
   print("dezembro")
 else:
  print("Digite de 1 a 12 Por favor!!")
