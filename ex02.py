6dc393f4de7c6b1b6bfa6efc8e2236c568e6bf16
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-04-07 11:42:39 -0300

    codigo novo

--- ex02.py
+++ ex02.py
@@ -1,4 +1,23 @@
-nome = input("Digite seu nome: ")
+#estrura de repetição#
+
+'''nome = input("Digite seu nome: ")
 for i in range(10,1,-2):
     print(nome)
     print(i)
+
+    for i in range(1,11):
+    print(i, end=" ")
+soma = 0
+for i in range(10):
+    num= int(input("digite o numero: "))
+    soma+=num
+print(f'soma total é {soma}')'''
+
+
+num= int(input("digite um numero : "))
+
+for i in range(1,11):
+    print(f"{num} x {i} = {i*num}")
+
+
+
